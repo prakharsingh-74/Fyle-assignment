28856c783a78c9ec801ad0c0380c5a68cb9e66dc
Prakhar Singh <prakhar22153054@akgec.ac.in>
2024-06-02 12:09:08 +0530

    what we do section updation

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
+let slideIndex = 0;
+showSlides();
+
+function currentSlide(n) {
+    slideIndex = n - 1; // Adjust slideIndex to be zero-based
+    showSlides();
+}
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("card");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 3000); // Change slide every 3 seconds
+}
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', function() {
     const textItems = document.querySelectorAll('.text-item');
     const mainImage = document.getElementById('main-image');
